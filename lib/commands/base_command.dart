d5fee0045e41e50eedb015317622c2b3afe07c1e
Shawn <shawn@shawnblais.com>
2020-09-09 11:34:47 -0600

    Update base_command.dart

--- lib/commands/base_command.dart
+++ lib/commands/base_command.dart
@@ -1,6 +1,5 @@
 
-// The commands will use this to access the Provided models and services.
-// This is just an example, you can store this anywhere you like, or do something like Commands.init(BuildContext)
+
 import 'package:flutter/material.dart';
 import 'package:mvcs_blogpost/models/app_model.dart';
 import 'package:mvcs_blogpost/models/user_model.dart';
@@ -8,7 +7,7 @@ import 'package:mvcs_blogpost/services/user_service.dart';
 import 'package:provider/provider.dart';
 
 BuildContext _mainContext;
-
+// The commands will use this to access the Provided models and services.
 void init(BuildContext c) => _mainContext = c;
 
 // Provide quick lookup methods for all the top-level models and services. Keeps the Command code slightly cleaner.
